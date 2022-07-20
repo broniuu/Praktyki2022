4e001a03f0deb80c75956068cd661674843b83b1
broniuu <86781217+broniuu@users.noreply.github.com>
2022-07-20 11:11:04 +0200

    update switch

--- 1.4Tablice/zad1/Program.cs
+++ 1.4Tablice/zad1/Program.cs
@@ -14,48 +14,71 @@ Console.WriteLine("Wypisanie elementu o podanym nr indexu (po wybraniu tej opcji
 Console.WriteLine("Zastąpienie elementu o podanym indexie, nowym elementem - 5");
 var taskNumber = int.Parse(Console.ReadLine());
 
+var newArray = new int[array.Length];
+int choosenIndex;
+switch (taskNumber)
+{
+    case 1:
+        newArray = array.OrderBy(x => x).ToArray();
+        WriteArray(newArray);
+        break;
+    case 2:
+        newArray = array.OrderByDescending(x => x).ToArray();
+        WriteArray(newArray);
+        break;
+    case 3:
+        var greatestElement = array.Max();
+        var smallestElement = array.Min();
+        Console.WriteLine($"Największy: {greatestElement}");
+        Console.WriteLine($"Najmniejszy: {smallestElement}");
+        break;
+    case 4:
+        Console.Write("Wyierz numer indeksu: ");
+        choosenIndex = int.Parse(Console.ReadLine());
+        Console.WriteLine($"element o podanym indeksie {array[choosenIndex]}");
+        Console.Write($"Dostępne indexy: ");
+        for (var i = 0; i < size; i++)
+        {
+            if (i != choosenIndex)
+            {
+                Console.Write($"{i} ");
+            }   
+        }
+        break;
+    case 5:
+        Console.Write("Wyierz numer indeksu: ");
+        choosenIndex = int.Parse(Console.ReadLine());
+        Console.Write("Wpisz nową wartość dla indeksu: ");
+        array[choosenIndex] = int.Parse(Console.ReadLine());
+        Console.Write("Zaktualizowana Tablica: ");
+        WriteArray(array);
+        break;
+    default:
+        Console.WriteLine("Wrong number");
+        break;
+}
 if (taskNumber == 1)
 {
-    var newArray = array.OrderBy(x => x).ToArray();
-    WriteArray(newArray);
+
 
 } else if (taskNumber == 2)
 {
-    var newArray = array.OrderByDescending(x => x).ToArray();
-    WriteArray(newArray);
+
 
 } else if (taskNumber == 3)
 {
-    var greatestElement = array.Max();
-    var smallestElement = array.Min();
-    Console.WriteLine($"Największy: {greatestElement}");
-    Console.WriteLine($"Najmniejszy: {smallestElement}");
+
 
 } else if (taskNumber == 4)
 {
-    Console.Write("Wyierz numer indeksu: ");
-    var choosenIndex = int.Parse(Console.ReadLine());
-    Console.WriteLine($"element o podanym indeksie {array[choosenIndex]}");
-    Console.Write($"Dostępne indexy: ");
-    for (var i = 0; i < size; i++)
-    {
-        if (i != choosenIndex)
-        {
-            Console.Write($"{i} ");
-        }   
-    }
+
 
 } else if (taskNumber == 5)
 {
-    Console.Write("Wyierz numer indeksu: ");
-    var choosenIndex = int.Parse(Console.ReadLine());
-    Console.Write("Wpisz nową wartość dla indeksu: ");
-    array[choosenIndex] = int.Parse(Console.ReadLine());
-    Console.Write("Zaktualizowana Tablica: ");
-    WriteArray(array);
+
 } else
 {
-    Console.WriteLine("Wrong number");
+    
 }
 
 static void WriteArray(IEnumerable<int> items)
