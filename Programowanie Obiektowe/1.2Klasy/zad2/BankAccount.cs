ebf6623ef4a4fdcc20ed56b5e0e627a84b2363cc
broniuu <86781217+broniuu@users.noreply.github.com>
2022-07-20 15:54:37 +0200

    initial commit

---
+++ Programowanie Obiektowe/1.2Klasy/zad2/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad23
+{
+    internal class BankAccount
+    {
+        public class SavingsAccount
+        {
+
+        }
+        public class SplitPayment
+        {
+
+        }
+    }
+}
---
+++ Programowanie Obiektowe/1.2Klasy/zad2/Pracownik.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Pracownik
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        private decimal Salary { get; set; }
+        private int VacationDays { get; set; }
+        private int UsedVacationDays { get; set; }
+
+
+        public Pracownik(string name,
+                         string surname,
+                         decimal salary,
+                         int vacationDays,
+                         int usedVacationDays)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Salary = salary;
+            this.VacationDays = vacationDays;
+            this.UsedVacationDays = usedVacationDays;
+        }
+
+        public int getUnusedVacationDays()
+        {
+            return this.VacationDays - this.UsedVacationDays;
+        }
+    }
+}
