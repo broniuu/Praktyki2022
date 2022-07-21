1eaf2e8f0f68f04be7624152ce14580963d6b7ec
broniuu <86781217+broniuu@users.noreply.github.com>
2022-07-21 13:37:58 +0200

    add 1.5 Wlasciwosci

---
+++ Programowanie Obiektowe/_1_4Metody/_1.6Konstruktor/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._6Konstruktor
+{
+    internal class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Race { get; set; }
+        public static int DogsCount = 0;
+        public Dog(string name, int age, string race)
+        {
+            Name = name;
+            Age = age;
+            Race = race;
+            DogsCount++;
+        }
+        public Dog(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Race = "Mongrel";
+            DogsCount++;
+        }
+
+    }
+}
--- Programowanie Obiektowe/_1_4Metody/_1.6Konstruktor/Program.cs
+++ Programowanie Obiektowe/_1_4Metody/_1.6Konstruktor/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _1._6Konstruktor;
+
+var dog1 = new Dog("Axa",2,"Jamnik");
+var dog2 = new Dog("Szarik", 4);
+var dog3 = new Dog("Burek", 10, "Owczarek");
+var count = Dog.DogsCount;
+
+
