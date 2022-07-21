bdc8bce226ca4dcc4a3276a54e8604d3f7bed5b2
broniuu <86781217+broniuu@users.noreply.github.com>
2022-07-21 09:51:35 +0200

    add metodyZad1

---
+++ Programowanie Obiektowe/_1_4Metody/ProgramowanieObiektowe/Program.cs
@@ -0,0 +1,22 @@
+﻿using ProgramowanieObiektowe;
+
+static int Sum(int a, int b)
+{
+    return a + b;
+}
+
+static string Unite(string a, string b)
+{
+    return string.Concat(a.Concat(b));
+}
+
+static Human MakeHuman(string name, string surname, DateTime bornDate)
+{
+    return new Human
+    {
+        Name = name,
+        Surname = surname,
+        BornDate = bornDate,
+    };
+}
+static void Nothing() { }
