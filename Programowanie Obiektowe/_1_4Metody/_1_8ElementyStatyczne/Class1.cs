27ee4adc41ba2fa1e7f541df83e065fb61ab9adf
broniuu <86781217+broniuu@users.noreply.github.com>
2022-07-22 08:58:45 +0200

    add 1.8 Elementy Statyczne

---
+++ Programowanie Obiektowe/_1_4Metody/_1_8ElementyStatyczne/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_8ElementyStatyczne
+{
+    internal class Class1
+    {
+        private static int Counter = 0;
+
+        public Class1(){ Counter++; }
+        static public void ShowNumberOfObjects()
+        {
+            Console.WriteLine(Counter);
+        }
+    }
+}
