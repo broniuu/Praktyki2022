a8e3ae9154002afb9ba7bc939d9b52863b0ea5cc
broniuu <86781217+broniuu@users.noreply.github.com>
2022-07-22 11:27:41 +0200

    add 1.0 Typy Wyliczeniowe

---
+++ Programowanie Obiektowe/_1_4Metody/1.10TypyWyliczeniowe/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._10TypyWyliczeniowe
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public ContractType ContractType { get; set; }
+    }
+    public enum ContractType
+    {
+        Trial,Intership,Temporaty,Other
+    }
+
+}
