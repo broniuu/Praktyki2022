f1f2813175c61aa05fd43c59d3e14ea78afd5611
broniuu <86781217+broniuu@users.noreply.github.com>
2022-07-25 12:33:53 +0200

    add 1.12 Roznica pomiedzy klasa a struktura

---
+++ Programowanie Obiektowe/_1_4Metody/_1_12RoznicaPomiedzyKlasaAFigura/Figura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_12RoznicaPomiedzyKlasaAStruktura
+{
+    public interface Figura
+    {
+        abstract public void WyswietlWzor();
+    }
+
+    public struct Kwadrat : Figura
+    {
+        public void WyswietlWzor()
+        {
+            Console.WriteLine("P = a * a");
+        }
+    }
+
+}
---
+++ Programowanie Obiektowe/_1_4Metody/_1_12RoznicaPomiedzyKlasaAFigura/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_12RoznicaPomiedzyKlasaAStruktura
+{
+    internal struct Number
+    {
+        public int number
+        {
+            get { return 20; }
+        }
+    }
+}
