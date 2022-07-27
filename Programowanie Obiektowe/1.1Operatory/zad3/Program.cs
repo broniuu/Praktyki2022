6c979a303266dab1c48fe12bac5690d4b1dcb2ec
broniuu <86781217+broniuu@users.noreply.github.com>
2022-07-27 13:47:04 +0200

    fix tautology

--- Programowanie Obiektowe/1.1Operatory/zad3/Program.cs
+++ Programowanie Obiektowe/1.1Operatory/zad3/Program.cs
@@ -6,5 +6,5 @@ var tautology1 = a == b || a != b ;
 Console.WriteLine(tautology1);
 var tautology2 = !(a > b && a <= b);
 Console.WriteLine(tautology2);
-var tautology3 = !false;
-Console.WriteLine((a != c && b != c) == (a != c || b != c));
+var tautology3 = (a != c && b != c) == (a != c || b != c);
+Console.WriteLine(tautology3);
