c9caebc739556aa00da3bf27cade8b850278fcba
broniuu <86781217+broniuu@users.noreply.github.com>
2022-07-27 07:55:49 +0200

    change directory names

---
+++ Programowanie Obiektowe/ProgramowanieObiektoweReszta/1.10TypyWyliczeniowe/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace _1._10TypyWyliczeniowe
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public ContractType ContractType { get; set; }
+    }
+    public enum ContractType
+    {
+        Trial, Intership, Temporaty, Other
+    }
+
+}
---
+++ Programowanie Obiektowe/ProgramowanieObiektoweReszta/_1_5ModyfikatoryDostepu/Cat.cs
@@ -0,0 +1,8 @@
+﻿using ClassLibrary;
+
+namespace _1_5ModyfikatoryDostepu
+{
+    internal class Cat : Animal
+    {
+    }
+}
